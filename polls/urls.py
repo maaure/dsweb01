1590bcaef550d208d607b954d4308dc9dd62e5bb
Maure Andrade <pedro.maure@gmail.com>
2023-05-11 14:00:32 +0000

    Criação das views

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     path("", views.polls_index, name="index"),
     path("old/", views.index, name="old_index"),
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pergunta_id>/", views.detail, name="detail"),
+    path("<int:pergunta_id>/vote/", views.vote, name="vote"),
+    path("<int:pergunta_id>/results/", views.results, name="results"),
 ]
-"""     
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"), """
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Pergunta, Alternativa
 
@@ -12,6 +13,32 @@ def polls_index(request):
     context = {"ultimas_questoes_cadastradas": ultimas_questoes_cadastradas}
     return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    pergunta = get_object_or_404(Pergunta, pk=question_id)
+def detail(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
     return render(request, "polls/detail.html", {"pergunta": pergunta})
+
+def vote(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    try:
+        selected_alternativa = pergunta.alternativa_set.get(pk=request.POST["alternativa"])
+    except (KeyError, Alternativa.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "pergunta": pergunta,
+                "mensagem_erro": "Você não escolheu uma alternativa.",
+            },
+        )
+    else:
+        selected_alternativa.votos += 1
+        selected_alternativa.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(pergunta.id,)))
+
+def results(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return render(request, "polls/results.html", {"pergunta": pergunta})
