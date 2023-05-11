c90283b845fed7430c12f33546e9f9de180521a4
Maure Andrade <pedro.maure@gmail.com>
2023-05-11 12:35:08 +0000

    :bricks: primeira versao dos modelos

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pergunta(models.Model):
+    titulo = models.CharField(max_length=200)
+    data_publicacao = models.DateTimeField('data publicacao')
+
+
+class Alternativa(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)# Create your models here.
