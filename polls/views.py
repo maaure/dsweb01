9041a3097b102b94e465fd3aeece387fce3a612a
Maure Andrade <pedro.maure@gmail.com>
2023-05-11 13:45:00 +0000

    Criação das views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Pergunta, Alternativa
 
 # Create your views here.
 def index(request):
     return HttpResponse("Pedro Maure Frutuoso de Andrade - 20221014040013")
+
+def polls_index(request):
+    ultimas_questoes_cadastradas = Pergunta.objects.order_by("-data_publicacao")[:5]
+    context = {"ultimas_questoes_cadastradas": ultimas_questoes_cadastradas}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    pergunta = get_object_or_404(Pergunta, pk=question_id)
+    return render(request, "polls/detail.html", {"pergunta": pergunta})
