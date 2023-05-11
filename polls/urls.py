6c77d51dca265a18458d2ea35436c58020ab0195
Maure Andrade <pedro.maure@gmail.com>
2023-05-11 14:20:19 +0000

    Adicionando views genericas

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.polls_index, name="index"),
     path("old/", views.index, name="old_index"),
-    path("<int:pergunta_id>/", views.detail, name="detail"),
-    path("<int:pergunta_id>/vote/", views.vote, name="vote"),
-    path("<int:pergunta_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Pergunta, Alternativa
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "ultimas_questoes_publicadas"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Pergunta.objects.order_by("-data_publi")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Pergunta
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Pergunta
+    template_name = "polls/results.html"
+
+
 def index(request):
     return HttpResponse("Pedro Maure Frutuoso de Andrade - 20221014040013")
-
-def polls_index(request):
-    ultimas_questoes_cadastradas = Pergunta.objects.order_by("-data_publicacao")[:5]
-    context = {"ultimas_questoes_cadastradas": ultimas_questoes_cadastradas}
-    return render(request, "polls/index.html", context)
-
-def detail(request, pergunta_id):
-    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
-    return render(request, "polls/detail.html", {"pergunta": pergunta})
 
 def vote(request, pergunta_id):
     pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
@@ -39,6 +49,17 @@ def vote(request, pergunta_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(pergunta.id,)))
 
+"""
+def polls_index(request):
+    ultimas_questoes_cadastradas = Pergunta.objects.order_by("-data_publicacao")[:5]
+    context = {"ultimas_questoes_cadastradas": ultimas_questoes_cadastradas}
+    return render(request, "polls/index.html", context)
+
+def detail(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return render(request, "polls/detail.html", {"pergunta": pergunta}) 
+
 def results(request, pergunta_id):
     pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
     return render(request, "polls/results.html", {"pergunta": pergunta})
+"""
