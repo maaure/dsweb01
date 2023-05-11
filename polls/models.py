39b301db67ae4681acc82d3e24ebe6a56bb2e99b
Maure Andrade <pedro.maure@gmail.com>
2023-05-11 12:40:10 +0000

    :feat: finalizado modelos

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Pergunta(models.Model):
     titulo = models.CharField(max_length=200)
     data_publicacao = models.DateTimeField('data publicacao')
-
+    def foi_publicada_recentemente(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Alternativa(models.Model):
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
